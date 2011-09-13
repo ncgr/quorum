65697dc5e06a8dd6d993c3be4e849256ba5b2d8b
Ken Seal <kas@ncgr.org>
2011-09-13 11:30:56 -0600

    Updated READMEs and added rake task quorum:blastdb:build.

--- lib/generators/quorum/install_generator.rb
+++ lib/generators/quorum/install_generator.rb
@@ -3,11 +3,12 @@ module Quorum
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../templates", __FILE__)
 
-      desc "Creates a Quorum initializer, quorum_settings.yml and run-blast " << 
-           "files."
+      desc "Creates a Quorum initializer, quorum_settings.yml and " <<
+           "executable files."
 
       def copy_initializer
-        template "quorum.rb", "config/initializers/quorum.rb"
+        template "quorum_initializer.rb", 
+          "config/initializers/quorum_initializer.rb"
         template "quorum_settings.yml", "config/quorum_settings.yml"
         template "quorum", "quorum/bin/quorum"
       end
@@ -15,6 +16,10 @@ module Quorum
       def change_file_permissions
         f = File.new("quorum/bin/quorum", "r")
         f.chmod(0755)
+      end
+
+      def add_mount_engine
+        route %Q(mount Quorum::Engine => "/quorum")
       end
 
       def show_readme

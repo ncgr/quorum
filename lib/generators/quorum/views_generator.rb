f4fdbf3f44292611b0a20f0497f432cb02f30850
Ken Seal <kas@ncgr.org>
2011-09-26 16:31:50 -0600

    Refactored quorum initializer, execution of system cmds and I18n locale.

---
+++ lib/generators/quorum/views_generator.rb
@@ -0,0 +1,19 @@
+module Quorum
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../../../app/views/quorum", __FILE__)
+      desc "Copy quorum views to your application."
+
+      VIEW_DIRECTORIES = [
+        "blasts"
+      ]
+
+      def copy_views
+        VIEW_DIRECTORIES.each do |d|
+          directory d.to_s, "app/views/quorum/#{d.to_s}"
+        end
+      end
+
+    end
+  end
+end
--- lib/quorum.rb
+++ lib/quorum.rb
@@ -1,4 +1,76 @@
 require "quorum/engine"
 
 module Quorum
+
+  mattr_accessor :blast_remote, :blast_ssh_host, :blast_ssh_user, 
+    :blast_ssh_options, :blast_script, :blast_log_dir, :blast_tmp_dir,
+    :blast_db, :tblastn, :blastp, :blastn, :blastx, :blast_threads
+
+  class << self
+    # Execute remotely.
+    def blast_remote
+      @@blast_remote || false
+    end
+
+    # Net::SSH host.
+    def blast_ssh_host
+      @@blast_ssh_host || nil
+    end
+
+    # Net::SSH user.
+    def blast_ssh_user
+      @@blast_ssh_user || nil
+    end
+
+    # Net::SSH options.
+    def blast_ssh_options
+      @@blast_ssh_options || {}
+    end
+
+    # Blast script path.
+    def blast_script
+      @@blast_script || nil
+    end
+
+    # Blast log dir path.
+    def blast_log_dir
+      @@blast_log_dir || nil
+    end
+
+    # Blast tmp dir path.
+    def blast_tmp_dir
+      @@blast_tmp_dir || nil
+    end
+
+    # Blast database path.
+    def blast_db
+      @@blast_db || nil
+    end
+
+    # tblastn directories.
+    def tblastn
+      @@tblastn || []
+    end
+
+    # blastp directories.
+    def blastp
+      @@blastp || []
+    end
+
+    # blastn directories.
+    def blastn
+      @@blastn || []
+    end
+
+    # blastx directories.
+    def blastx
+      @@blastx || []
+    end
+
+    # Number of Blast threads.
+    def blast_threads
+      @@blast_threads || 1
+    end
+  end
+
 end

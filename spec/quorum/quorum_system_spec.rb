df0c7e27004f5b12f6e99ca5a7fcf8a954cc5c02
Ken Seal <kas@ncgr.org>
2011-10-17 12:10:11 -0600

    Added specs.

--- spec/quorum/quorum_system_spec.rb
+++ spec/quorum/quorum_system_spec.rb
@@ -5,5 +5,20 @@ describe "Quorum::System" do
   describe "#execute_cmd" do
     include Quorum::System
 
+    before(:all) do
+      @args = {
+        :cmd      => "whoami >& /dev/null",
+        :remote   => false,
+        :ssh_host => "example.com",
+        :ssh_user => "user"
+      }
+    end
+
+    it "executes cmd locally via Kernel#system when remote = false" do
+      execute_cmd(
+        @args[:cmd], @args[:remote], @args[:ssh_host], @args[:ssh_user]
+      ).should eq(:error_0)
+    end
+
   end
 end

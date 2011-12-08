75440b566de022f5e2b4cc5c5f896775d16e5519
Ken Seal <kas@ncgr.org>
2011-12-08 10:01:52 -0700

    Updated specs.

--- spec/models/blastx_job_report_spec.rb
+++ spec/models/blastx_job_report_spec.rb
@@ -6,4 +6,8 @@ describe Quorum::BlastxJobReport do
     Quorum::BlastxJobReport.methods.should include(:default_order) 
   end
 
+  it "should respond to default_order" do
+    Quorum::BlastxJobReport.methods.should include(:by_query) 
+  end
+
 end
--- spec/models/tblastn_job_report_spec.rb
+++ spec/models/tblastn_job_report_spec.rb
@@ -6,4 +6,8 @@ describe Quorum::TblastnJobReport do
     Quorum::TblastnJobReport.methods.should include(:default_order) 
   end
 
+  it "should respond to default_order" do
+    Quorum::TblastnJobReport.methods.should include(:by_query) 
+  end
+
 end

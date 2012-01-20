961c318aa58cf8f6fd7e7b2b7b20c00b3a9b0232
Ken Seal <kas@ncgr.org>
2012-01-20 10:51:10 -0700

    Added Colors to Quorum::Helpers. Print install generator warning message in red.

--- lib/quorum/helpers.rb
+++ lib/quorum/helpers.rb
@@ -12,6 +12,76 @@ module Quorum
       message = I18n.t("#{kind}", options)
       flash[key] = message if message.present?
     end
- 
+
+    #
+    # Colorize console text.
+    #
+    module Colors
+      ## Thanks to the Rspec team for this code! ##
+
+      #
+      # Main color method.
+      #
+      def color(text, color_code)
+        "#{color_code}#{text}\e[0m"
+      end
+
+      #
+      # Bold
+      #
+      def bold(text)
+        color(text, "\e[1m")
+      end
+
+      #
+      # White
+      #
+      def white(text)
+        color(text, "\e[37m")
+      end
+
+      #
+      # Green
+      #
+      def green(text)
+        color(text, "\e[32m")
+      end
+
+      #
+      # Red
+      #
+      def red(text)
+        color(text, "\e[31m")
+      end
+
+      #
+      # Magenta
+      #
+      def magenta(text)
+        color(text, "\e[35m")
+      end
+
+      #
+      # Yellow
+      #
+      def yellow(text)
+        color(text, "\e[33m")
+      end
+
+      #
+      # Blue
+      #
+      def blue(text)
+        color(text, "\e[34m")
+      end
+
+      #
+      # Grey
+      #
+      def grey(text)
+        color(text, "\e[90m")
+      end
+    end
+
   end
 end

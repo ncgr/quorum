dbd8c82f5a19daeffd197119cf328aa1e2b58f75
Ken Seal <kas@ncgr.org>
2012-01-27 12:00:08 -0700

    Configured Jasmine for testing and added spec.

--- spec/javascripts/string_spec.js
+++ spec/javascripts/string_spec.js
@@ -1,7 +1,13 @@
 describe("String", function() {
 
   it("truncates a string to length n", function() {
-    
+    var str = "this is a long string.";
+    expect(str.trunc(5)).toEqual("this...");    
+  });
+
+  it("doesn't truncate string when length is less than n", function() {
+    var str = "this is a long string.";
+    expect(str.trunc(50)).toEqual(str);
   });
 
 });

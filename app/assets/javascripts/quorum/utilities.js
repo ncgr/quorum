af819fede68c7ce08de1ce6344a9798872c2790e
Ken Seal <kas@ncgr.org>
2012-01-27 12:00:08 -0700

    Configured Jasmine for testing and added spec.

--- app/assets/javascripts/quorum/utilities.js
+++ app/assets/javascripts/quorum/utilities.js
@@ -5,7 +5,12 @@ String.prototype.trunc = function(n) {
   var longStr = this.length > n;
   var str     = longStr ? this.substr(0, n-1) : this;
 
-  longStr ? str.substr(0, str.lastIndexOf(' ')) : str;
+  if (longStr) {
+    str.substr(0, str.lastIndexOf(' '));
+  } else {
+    str;
+  }
+
   return longStr ? str + '...' : str;
 }
 

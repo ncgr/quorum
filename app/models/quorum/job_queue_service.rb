d01071ce8622ac1edc89125f7bce4c6cbc5a034e
Ken Seal <kas@ncgr.org>
2012-11-27 10:42:39 -0700

    Updated search command to prep for gmap.

--- app/models/quorum/job_queue_service.rb
+++ app/models/quorum/job_queue_service.rb
@@ -5,24 +5,24 @@ module Quorum
     # Queue search workers.
     #
     def self.queue_search_workers(job)
-      jobs = []
+      blast_jobs = []
       if job.blastn_job && job.blastn_job.queue
-        jobs << Workers::System.create_search_command("blastn", job.id)
+        blast_jobs << Workers::System.create_search_command("blastn", job.id)
       end
       if job.blastx_job && job.blastx_job.queue
-        jobs << Workers::System.create_search_command("blastx", job.id)
+        blast_jobs << Workers::System.create_search_command("blastx", job.id)
       end
       if job.tblastn_job && job.tblastn_job.queue
-        jobs << Workers::System.create_search_command("tblastn", job.id)
+        blast_jobs << Workers::System.create_search_command("tblastn", job.id)
       end
       if job.blastp_job && job.blastp_job.queue
-        jobs << Workers::System.create_search_command("blastp", job.id)
+        blast_jobs << Workers::System.create_search_command("blastp", job.id)
       end
 
-      unless jobs.blank?
-        jobs.each do |j|
+      unless blast_jobs.blank?
+        blast_jobs.each do |b|
           Workers::System.enqueue(
-            j,
+            b,
             Quorum.blast_remote,
             Quorum.blast_ssh_host,
             Quorum.blast_ssh_user,

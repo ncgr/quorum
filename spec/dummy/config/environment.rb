59f5649457024dcb591e6624fde6ef0e07d7ca82
Ken Seal <kas@ncgr.org>
2012-11-28 14:01:36 -0700

    Enabled DB env for TravisCI.
    * DBs: MySQL, PostgreSQL

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -4,17 +4,30 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Dummy::Application.initialize!
 
-## Explicitly remove AR transactions during tests ##
+## Explicitly remove MySQL AR transactions during tests ##
 if Rails.env.test?
   # Force the loading of AR stuff
   ActiveRecord::Base.connection.execute('SELECT 1')
 
-  # Remove transactions
-  ActiveRecord::ConnectionAdapters::Mysql2Adapter.class_eval do
-    def begin_db_transaction
-    end
+  if ENV['DB'] == 'mysql'
+    # Remove transactions
+    ActiveRecord::ConnectionAdapters::Mysql2Adapter.class_eval do
+      def begin_db_transaction
+      end
 
-    def commit_db_transaction
+      def commit_db_transaction
+      end
+    end
+  end
+
+  if ENV['DB'] == 'postgresql'
+    # Remove transactions
+    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+      def begin_db_transaction
+      end
+
+      def commit_db_transaction
+      end
     end
   end
 end
